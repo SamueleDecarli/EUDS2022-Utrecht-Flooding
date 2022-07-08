ce0ccd09a46ec11c0185d02e8edd968d4ea51570
Daniel Laumer <dan11546@esri.com>
2022-07-08 14:04:09 -0700

    graphics layer and added steps

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,24 @@ import RainyWeather from "@arcgis/core/views/3d/environment/RainyWeather";
 
 import "@esri/calcite-components/dist/calcite/calcite.css";
 import "@esri/calcite-components/dist/components/calcite-loader";
-import { FalseLiteral } from "typescript";
+import { FalseLiteral, visitLexicalEnvironment } from "typescript";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 import Layer from "@arcgis/core/layers/Layer";
 import Home from "@arcgis/core/widgets/Home";
 import Camera from "@arcgis/core/Camera";
 import GroupLayer from "@arcgis/core/layers/GroupLayer";
 import Legend from "@arcgis/core/widgets/Legend";
+import FeatureFilter from "@arcgis/core/layers/support/FeatureFilter";
+import FeatureLayerView from "@arcgis/core/views/layers/FeatureLayerView";
+import Polygon from "@arcgis/core/geometry/Polygon";
+import SpatialReference from "@arcgis/core/geometry/SpatialReference";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
+import { WaterSymbol3DLayer } from "@arcgis/core/symbols";
+import PolygonSymbol3D from "@arcgis/core/symbols/PolygonSymbol3D";
+import Graphic from "@arcgis/core/Graphic";
+import SimpleRenderer from "@arcgis/core/renderers/SimpleRenderer";
+import { colorEqual } from "@esri/calcite-components/dist/types/components/color-picker/utils";
+import FillSymbol3DLayer from "@arcgis/core/symbols/FillSymbol3DLayer";
 
 
 /***********************************
@@ -57,6 +68,60 @@ const view = new SceneView({
   },
 });
 
+
+//***********************************
+//* Step 1: Add Flooding data
+//***********************************
+
+const flood = new FeatureLayer({
+  url: "https://services2.arcgis.com/cFEFS0EWrhfDeVw9/arcgis/rest/services/FloodingDataUtrecht/FeatureServer/0",
+  title: "Flood level layer",
+  elevationInfo: {
+    mode: "absolute-height"
+  },
+  maxScale: 0,
+  minScale: 0,
+  renderer: new SimpleRenderer({
+    symbol: new PolygonSymbol3D({
+      symbolLayers: [
+        new FillSymbol3DLayer({
+          material: { color: "orange" }
+        })
+      ]
+    })
+  })
+});
+
+//view.map.add(flood)
+
+
+//***********************************
+//* Step 2: Style flooding data
+//***********************************
+
+let rendererWater = new SimpleRenderer({
+  symbol: new PolygonSymbol3D({
+    symbolLayers: [
+      new WaterSymbol3DLayer({
+        color: [194, 152, 110, 0.77],
+        waveStrength: "rippled"
+      })
+    ]
+  })
+})
+
+//flood.renderer = rendererWater
+
+
+//***********************************
+//* Step 3: Finalize app
+//***********************************
+
+//finalizeApp()
+
+
+
+
 /***********************************
  * Add the widgets' UI elements to the view
  ***********************************/
@@ -77,7 +142,7 @@ const daylightExpand = new Expand({
 });
 view.ui.add([weatherExpand, daylightExpand], "top-right");
 view.ui.add(new Home({ view: view }), "top-left")
-const legend = new Expand({ view: view, expanded: false, content: new Legend({view:view})})
+const legend = new Expand({ view: view, expanded: false, content: new Legend({ view: view }) })
 view.ui.add(legend, "bottom-left")
 
 /***********************************
@@ -86,6 +151,8 @@ view.ui.add(legend, "bottom-left")
 // Wait for the view to be loaded, in order to being able to retrieve the layer
 let floodLevel: Layer;
 let floodImpact: Layer;
+let floodLevelLayer: FeatureLayer;
+let floodLayer: GraphicsLayer;
 
 const flooding1 = document.getElementById(
   "flooding1"
@@ -107,6 +174,65 @@ view.when(() => {
   floodLevel = scene.allLayers.find(function (layer) {
     return layer.title === "Flood Level";
   });
+
+  floodLevelLayer = ((floodLevel as GroupLayer).layers.getItemAt(1) as FeatureLayer)
+
+  view.whenLayerView(floodLevelLayer).then((layerView) => {
+    let filterPolygon = new Polygon({
+      spatialReference: new SpatialReference({ wkid: 102100 }),
+      rings: [[[569667.6175085438, 6817221.788167432, 4.288249521990564],
+      [570338.5404688591, 6817144.114795292, 4.288249522447586],
+      [570276.2118804371, 6816697.20946292, 4.288249524310231],
+      [569644.7564071192, 6816759.350547839, 4.288249522447586],
+      [569667.6175085438, 6817221.788167432, 4.288249521990564]]]
+    });
+    /*
+    (layerView as FeatureLayerView).filter = new FeatureFilter({
+      geometry: filterPolygon,
+      spatialRelationship: "contains",
+      distance: 0,
+      units: "meters"
+    });
+    */
+
+    let waterSymbol = new PolygonSymbol3D({
+      symbolLayers: [
+        new WaterSymbol3DLayer({
+          color: [194, 152, 110, 0.77],
+          waveStrength: "rippled"
+        })
+      ]
+    })
+    floodLayer = new GraphicsLayer({});
+
+
+    var query = floodLevelLayer.createQuery();
+    query.geometry = filterPolygon,
+      query.spatialRelationship = "contains"
+
+    floodLevelLayer.queryFeatures(query).then((results) => {
+      let features = results.features
+      if (features.length > 0) {
+        for (let i = 0; i < features.length; i++) {
+          console.log(features[i].geometry);
+          let graphic = new Graphic({
+            geometry: features[i].geometry,
+            symbol: waterSymbol
+          });
+          floodLayer.add(graphic);
+        }
+      }
+    })
+    floodLayer.elevationInfo = {
+      mode: "absolute-height",
+      offset: 3.6
+    }
+    floodLayer.visible = false;
+    view.map.add(floodLayer);
+    view.map.remove(floodLevel);
+
+  })
+
 
   floodImpact = scene.allLayers.find(function (layer) {
     return layer.title === "Building Flood Impact";
@@ -144,38 +270,37 @@ view.when(() => {
   });
 });
 
+
 function changeFlooding(value: number) {
   switch (value) {
     case 1:
       view.environment.weather = new CloudyWeather({
         cloudCover: 0.5,
       });
-      floodLevel.visible = false;
+      floodLayer.visible = false;
       break;
     case 2:
       view.environment.weather = new RainyWeather({
         cloudCover: 0.4,
         precipitation: 0.2,
       });
-      floodLevel.visible = false;
+      floodLayer.visible = false;
       break;
     case 3:
       view.environment.weather = new RainyWeather({
         cloudCover: 0.4,
         precipitation: 0.5,
       });
-      floodLevel.visible = true;
-      ((floodLevel as GroupLayer).layers.getItemAt(0) as FeatureLayer).elevationInfo = { mode: 'absolute-height', offset: 2 };
-      ((floodLevel as GroupLayer).layers.getItemAt(1) as FeatureLayer).elevationInfo = { mode: 'absolute-height', offset: -0.5 };
+      floodLayer.visible = true;
+      floodLayer.elevationInfo = { mode: 'absolute-height', offset: 3.1 };
       break;
     case 4:
       view.environment.weather = new RainyWeather({
         cloudCover: 0.4,
         precipitation: 1,
       });
-      floodLevel.visible = true;
-      ((floodLevel as GroupLayer).layers.getItemAt(0) as FeatureLayer).elevationInfo = { mode: 'absolute-height', offset: 3 };
-      ((floodLevel as GroupLayer).layers.getItemAt(1) as FeatureLayer).elevationInfo = { mode: 'absolute-height', offset: 0 };
+      floodLayer.visible = true;
+      floodLayer.elevationInfo = { mode: 'absolute-height', offset: 3.6 };
       break;
   }
 }
@@ -194,5 +319,15 @@ impact.addEventListener("click", () => {
     legend.expanded = false;
   }
 });
+
+function finalizeApp() {
+
+  document.getElementById("sliderContainer")!.style.display = "flex"
+  document.getElementById("labelContainer")!.style.display = "flex"
+  document.getElementById("buttonContainer")!.style.display = "flex"
+
+
+  view.map.remove(flood);
+}
 
 window["view"] = view;
